7d86e97317dd4d125e11586ef5948105c319c124
Joel Pillar-Rogers <jdpillar@gmail.com>
2022-11-15 00:42:14 +1030

    finished command line grok program

--- grok/src/main.rs
+++ grok/src/main.rs
@@ -11,15 +11,15 @@ use grok::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(&args);
+    //dbg!(&args);
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = grok::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
