b3b86f9163ba6595b2fb22c478f4fee36367acfa
Joel Pillar-Rogers <jdpillar@gmail.com>
2022-11-14 23:00:45 +1030

    finished the testing chapter, ready to start on the IO project

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -2,81 +2,101 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
+pub fn add_two(a: i32) -> i32 {
+    a + 2
 }
 
-impl Rectangle {
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
-}
+// #[derive(Debug)]
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
 
-pub struct Guess {
-    value: i32,
-}
+// impl Rectangle {
+//     fn can_hold(&self, other: &Rectangle) -> bool {
+//         self.width > other.width && self.height > other.height
+//     }
+// }
 
-impl Guess {
-    pub fn new(value: i32) -> Guess {
-        if value < 1 {
-            panic!(
-                "Guess value must be greater than or equal to 1, got {}.",
-                value
-            );
-        } else if value > 100 {
-            panic!(
-                "Guess value must be less than or equal to 100, got {}.",
-                value
-            );
-        }
+// pub struct Guess {
+//     value: i32,
+// }
 
-        Guess { value }
-    }
-}
+// impl Guess {
+//     pub fn new(value: i32) -> Guess {
+//         if value < 1 {
+//             panic!(
+//                 "Guess value must be greater than or equal to 1, got {}.",
+//                 value
+//             );
+//         } else if value > 100 {
+//             panic!(
+//                 "Guess value must be less than or equal to 100, got {}.",
+//                 value
+//             );
+//         }
+
+//         Guess { value }
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-
-    #[test]
-    fn it_works2() -> Result<(), String> {
-        if 2 + 2 == 4 {
-            Ok(())
-        } else {
-            Err(String::from("two plus two does not equal four"))
-        }
-    }
+    // #[test]
+    // fn it_works() {
+    //     let result = add(2, 2);
+    //     assert_eq!(result, 4);
+    // }
 
     // #[test]
-    // fn it_fails() {
-    //     panic!("make this test fail!");
+    // fn it_works2() -> Result<(), String> {
+    //     if 2 + 2 == 4 {
+    //         Ok(())
+    //     } else {
+    //         Err(String::from("two plus two does not equal four"))
+    //     }
+    // }
+
+    // // #[test]
+    // // fn it_fails() {
+    // //     panic!("make this test fail!");
+    // // }
+
+    // #[test]
+    // fn larger_can_hold_smaller() {
+    //     let larger = Rectangle {
+    //         width: 8,
+    //         height: 7,
+    //     };
+    //     let smaller = Rectangle {
+    //         width: 5,
+    //         height: 1,
+    //     };
+
+    //     assert!(larger.can_hold(&smaller));
+    // }
+
+    // #[test]
+    // #[should_panic(expected = "less than or equal to 100")]
+    // fn greater_than_100() {
+    //     Guess::new(200);
     // }
 
     #[test]
-    fn larger_can_hold_smaller() {
-        let larger = Rectangle {
-            width: 8,
-            height: 7,
-        };
-        let smaller = Rectangle {
-            width: 5,
-            height: 1,
-        };
-
-        assert!(larger.can_hold(&smaller));
+    fn add_two_and_two() {
+        assert_eq!(4, add_two(2));
     }
 
     #[test]
-    #[should_panic(expected = "less than or equal to 100")]
-    fn greater_than_100() {
-        Guess::new(200);
+    #[ignore]
+    fn add_three_and_two() {
+        assert_eq!(5, add_two(3));
+    }
+
+    #[test]
+    fn one_hundred() {
+        assert_eq!(102, add_two(100));
     }
 }
---
+++ adder/tests/integration_test.rs
@@ -0,0 +1,7 @@
+
+mod common;
+
+#[test]
+fn it_adds_two() {
+    assert_eq!(4, adder::add_two(2));
+}
