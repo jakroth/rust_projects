ce2489bc4b62674d0f6b91a4184883cd61a1c3cc
Joel Pillar-Rogers <jdpillar@gmail.com>
2025-04-15 18:51:33 +0930

    up to smart pointers

--- rustbook/backyard/src/garden.rs
+++ rustbook/backyard/src/garden.rs
@@ -1,1 +1,5 @@
+//! # My Crate
+//!
+//! `my_crate` is a collection of utilities to make performing certain
+//! calculations more convenient.
 pub mod vegetables;
--- rustbook/backyard/src/garden/vegetables.rs
+++ rustbook/backyard/src/garden/vegetables.rs
@@ -1,2 +1,12 @@
+/// Adds one to the number given.
+///
+/// # Examples
+///
+/// ```
+/// let arg = 5;
+/// let answer = my_crate::add_one(arg);
+///
+/// assert_eq!(6, answer);
+/// ```
 #[derive(Debug)]
 pub struct Asparagus {}
